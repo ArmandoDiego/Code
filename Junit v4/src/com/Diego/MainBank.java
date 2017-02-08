53d8765e151c189e79d21772a879a5cfde05ff0a
ArmandoDiego <HP@HP-PC>
2017-02-07 21:50:56 -0600

    GetIn

---
+++ Junit v4/src/com/Diego/MainBank.java
@@ -0,0 +1,9 @@
+package com.Diego;
+
+public class MainBank {
+
+    public static void main(String[] args) {
+        Utilities util = new Utilities();
+        util.removePairs("AABCDDEFF");
+    }
+}
